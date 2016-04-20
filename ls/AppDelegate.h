7bf71fa2e50f388f13fcc7c81c9f12138f33394d
Yirenduishou <zJob_shuang@163.com>
2016-04-20 11:23:57 +0800

    update

---
+++ ls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ls
+//
+//  Created by SkyAndSea on 16/4/20.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ls
+//
+//  Created by SkyAndSea on 16/4/20.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
